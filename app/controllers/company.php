76b841eafcfa7674a9da3af85692711e7d6e9909
Phạm Đức Đạt <91515103+Dominic2k@users.noreply.github.com>
2024-12-01 19:09:26 +0700

    update

--- app/controllers/company.php
+++
@@ -1,9 +0,0 @@
-<?php
-class company extends DController{
-        public function __construct() {
-        $data = array();
-        $message = array();
-        
-        parent::__construct();
-    }
-}
